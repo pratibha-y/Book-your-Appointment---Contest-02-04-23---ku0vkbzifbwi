57d517c57819b8176f9a591e5e84aeda2d94a7ba
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-02 14:01:18 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -4,15 +4,15 @@ const initialState = {
   appointments: [],
 };
 
-function appointmentReducer(state = initialState, action) {
+function appointmentReducer(state = initialState, action) {state =initialState,action){
   switch (action.type) {
     case 'ADD_APPOINTMENT':
-      return {};
+      return {appointments:[...state.appointments,action.payload]};
     default:
       return state;
   }
 }
 
-const store = createStore();
+const store = createStore(appointmentReducer);
 
 export default store;
